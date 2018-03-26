4ee4aa41ec106d3a45512e4f61410b31f80a8215
unknown <luanrochaoliveira@gmail.com>
2018-03-26 19:51:52 -0300

    Inclusao da validacao do CNPJ

--- CalculaDvCnpj/src/test/java/com/example/demo/CnpjTest.java
+++ CalculaDvCnpj/src/test/java/com/example/demo/CnpjTest.java
@@ -4,87 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import br.gov.serpro.quitacao.dominio.modelo.erro.ErrosQuitacao.CnpjInvalido;
+
 public class CnpjTest {
 
-	//@Test
-	public void test() {
-	Cnpj validadorCnpj = new Cnpj();
-	String cnpjCemFormatacao = "03.014.284/0001-63";
-	String cnpjSemFormatacao = "03014284000163";
-	
-	System.out.println(validadorCnpj.isValidCNPJ(cnpjSemFormatacao));
-	
-	
-	
-	
-	}
-	
-	@Test
-	public void valida_cnpj_com_formatacao() {
-	ValidaCnpj validadorCnpj = new ValidaCnpj();
-	String cnpjValidado = null;
-	String cnpjComFormatacao = "03.014.284/0001-63";
-	assertEquals("true", ""+validadorCnpj.isCNPJ(cnpjComFormatacao));
-	String cnpjEsperado = "03.014.284/0001-63";
-	boolean isValid =  validadorCnpj.isCNPJ(cnpjComFormatacao);
-	if(isValid) {
-		cnpjValidado = validadorCnpj.retiraFormatacaoCnpj(cnpjEsperado);
-	}
-	String cnpjRetornado = validadorCnpj.formataCnpj(cnpjValidado);
-	
-	assertEquals(cnpjEsperado, cnpjRetornado);
-	System.out.println("E para retornar um boolean: "+validadorCnpj.isCNPJ(cnpjComFormatacao));
-	//System.out.println("E para retornar o CNPJ Formatado"+validadorCnpj.mascaraCnpj(cnpjSemFormatacao));
-	
-	
-	
-	
-	}
-	
 	@Test
 	public void valida_cnpj_sem_formatacao() {
-		ValidaCnpj validadorCnpj = new ValidaCnpj();
-		String cnpjSemFormatacao = "03014284000163";
-		assertEquals("true", ""+validadorCnpj.isCNPJ(cnpjSemFormatacao));
-		assertEquals("03.014.284/0001-63", ""+validadorCnpj.formataCnpj(cnpjSemFormatacao));
-	
-	System.out.println(validadorCnpj.isCNPJ(cnpjSemFormatacao));
-	System.out.println(validadorCnpj.formataCnpj(cnpjSemFormatacao));
-	
-	
+		String cnpjInformado = "03014284000163";
+		String cnpjEsperado = "03.014.284/0001-63";
+		System.out.println(Cnpj.novo(cnpjInformado).toString());
+		System.out.println(Cnpj.novo(cnpjInformado));
+
+		assertEquals(cnpjEsperado, ""+Cnpj.novo(cnpjInformado));
+		
 	}
 	
-	@Test
-	public void valida_cnpj_com_zeros_a_frente_formatacao() {
-		ValidaCnpj validadorCnpj = new ValidaCnpj();
-		String cnpjSemFormatacao = "00000000000191";
-		assertEquals("true", ""+validadorCnpj.isCNPJ(cnpjSemFormatacao));
-		assertEquals("00.000.000/0001-91", ""+validadorCnpj.formataCnpj(cnpjSemFormatacao));
-	
-	System.out.println(validadorCnpj.isCNPJ(cnpjSemFormatacao));
-	System.out.println(validadorCnpj.formataCnpj(cnpjSemFormatacao));
-	
-	
-	}
-	
-	@Test
-	public void valida_conversao_cnpj_para_inteiro() {
-		ValidaCnpj validadorCnpj = new ValidaCnpj();
-		String cnpjValidado = null;
-		String cnpjSemFormatacao = "00000000000191";
-		boolean isValid =  validadorCnpj.isCNPJ(cnpjSemFormatacao);
-		if(isValid) {
-			cnpjValidado = cnpjSemFormatacao;
-		}
+	@Test(expected=CnpjInvalido.class)
+	public void valida_cnpj_excecao() {
+		String cnpjInformado = "03014284000162";
+		String cnpjEsperado = "03.014.284/0001-63";
+		assertEquals(cnpjEsperado, Cnpj.novo(cnpjInformado).toString());
 		
-	 Integer cnpjEsperado = 191;
-		
-		//assertEquals(00000000000191L, validadorCnpj.converteCnpjParaNumerico(cnpjValidado), 0.0);
-		assertEquals(cnpjEsperado, validadorCnpj.converteCnpjParaNumerico(cnpjValidado));
-	
-	
-	
-	
 	}
 
 }
